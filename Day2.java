ff926b6ba3e804b6907707be5437844ae4716270
Rasti2319 <rastih54@gmail.com>
2025-09-09 15:09:55 +0800

    Day2.java

---
+++ Day2.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.print("Rasti");
+		System.out.println("hay");
+		System.out.printf("nama saya %s%n berumur %d%n", "Rasti",19);
+	}
+}
