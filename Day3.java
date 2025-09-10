cfc9caef7c47470f36e637d8d3d0a0f3f943f0da
Rasti2319 <rastih54@gmail.com>
2025-09-10 22:27:24 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Rasti");
+		//ini untuk komentar//
+		
+	/*ini untuk komentar,jika lebih panjang bisa lebih dari satu baris*/
+	
+		
+	}
+}
