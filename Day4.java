5a007177f77b6f2d4f9ebd5bb58938a59f436551
Rasti2319 <rastih54@gmail.com>
2025-09-11 20:25:07 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Halo dunia!\nSelamat datang di java");
+		System.out.println("Nama\t\tRasti");
+		System.out.println("Umur\t\t19");
+		System.out.println("Hobby\t\tMasak");
+	}
+}
