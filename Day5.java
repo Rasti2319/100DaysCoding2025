74aaa778cbfcc69a3d41f7d6ecf3b70f8174bff8
Rasti2319 <rastih54@gmail.com>
2025-09-12 17:50:58 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		byte a = 20 ;
+		short b = 300 ;
+		int c = 5000 ;
+		long d = 600000 ;
+		
+		System.out.println(a) ;
+		System.out.println(b) ;
+		System.out.println(c) ;
+		System.out.println(d) ;
+	}
+}
