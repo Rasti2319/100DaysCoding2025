a8d10683e9f3535e0993e362ca9e730201c8cfd4
Rasti2319 <rastih54@gmail.com>
2025-09-13 20:21:45 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		
+		
+		double a = 2.222222 ;  
+		float b = 9.9999f ;
+		
+		System.out.println("double ="+ a );
+		System.out.println("float ="+ b );
+		
+
+	}
+}
