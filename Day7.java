22e44bdf622036b6266cd2c1cf40a2b353c8309b
Rasti2319 <rastih54@gmail.com>
2025-09-14 20:54:08 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		char sampleOfChar = 'R' ;
+		boolean sampleOfBoolean = true ;
+		System.out.println("Nilai char : " + sampleOfChar) ;
+		System.out.println("Nilai boolean : " + sampleOfBoolean) ;
+		
+		
+	}
+}
