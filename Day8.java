aa7b91ee0bf0a04ef7f385d63a2e3338063e5451
Rasti2319 <rastih54@gmail.com>
2025-09-15 20:58:20 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		String pesan ="Selamat belajar";
+		String Nama ="Rasti";
+		String Nomor ="231023";
+		
+		System.out.println("String :" + pesan );
+		System.out.println("String :" + Nama );
+		System.out.println("String :" + Nomor);
+	}
+}
