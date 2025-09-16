aff6a697b210db10bc96fbebab600dab85e25cc1
Rasti2319 <rastih54@gmail.com>
2025-09-16 22:31:28 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		String nama = "Rasti" ;
+		int umur = 18 ;
+		 
+		 nama = "Rasti Agus" ;
+		 umur  = 19 ;
+		 
+		 System.out.println("Nama awal : " + nama ) ;
+		 System.out.println("Umur awal : " + umur );         System.out.println("Nama update : " + nama ) ;
+         System.out.println("Umur update : " + umur ) ;
+		
+	}
+}
