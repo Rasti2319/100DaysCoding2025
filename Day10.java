8623b11052851e9b908093461e6b58a955bfa210
Rasti2319 <rastih54@gmail.com>
2025-09-17 19:17:17 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+	//variabel yamg nilainya dapat berubah ubah
+		int umur=18;
+		System.out.println("umur=" + umur);
+		umur=19;
+		System.out.println("umur=" + umur);
+		//konstanta nilainya tidak dapat berubah-ubah
+		final int UMUR=20;
+		System.out.println("konstanta =" +UMUR);
+
+	
+			}
+
+}
