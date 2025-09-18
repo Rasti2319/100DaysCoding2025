a29d008ac7fc3214f9d8da1c86d9cfb56b73a12d
Rasti2319 <rastih54@gmail.com>
2025-09-18 19:20:11 +0800

    Day11.java

---
+++ Day11.java
@@ -0,0 +1,20 @@
+ import java.util.Scanner ;
+       public class Main {
+       
+		public static void main(String[] args) {
+			Scanner bt = new Scanner(System.in);
+        System.out.println("Masukkan nama");
+        String nama= bt.next();
+        System.out.println("Masukkan umur");
+        int umur= bt.nextInt();
+        System.out.println("Masukkan alamat");
+        String alamat= bt.next();
+        
+        System.out.println("nama");
+        System.out.println("umur");
+        System.out.println("alamat");
+        
+        
+    }
+    
+       }
