110cffe2c5dd4cf1d97ac39284e7538647df7309
Rasti2319 <rastih54@gmail.com>
2025-09-20 19:48:59 +0800

    Day13.java

---
+++ Day13.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		 String nama="sty";
+		 int umur=19;
+	     double ipk=3.37;
+		 System.out.println("nama :"+ nama);
+		 System.out.print("umur :"+ umur);
+		 System.out.printf("nama :%s ,umur :%d ,ipk :%f" , nama , umur , ipk);
+		 
+	}
+}
