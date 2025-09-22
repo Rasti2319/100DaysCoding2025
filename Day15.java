17a564ba8cacfa7059192778706e2eeddabe17c9
Rasti2319 <rastih54@gmail.com>
2025-09-22 20:39:58 +0800

    Day15.java

---
+++ Day15.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		int a=17;
+		int b=17;
+		int c=4;
+		int hasil = a + b - c;
+		System.out.println("penjumlahan + pengurangan=" + hasil);
+	}
+}
