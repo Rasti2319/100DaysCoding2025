5fae579d1389f83dc6ca67f4c5f63a57870b84ee
Rasti2319 <rastih54@gmail.com>
2025-09-23 16:51:18 +0800

    Day16.java

---
+++ Day16.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		int a = 4;
+		int b = 3;
+		int c = 2;
+		
+		int hasil = a*b/c;
+		System.out.println("perkalian + pembagian=" + hasil);
+		
+	}
+}
