a9cdef5272a00e71378fa75720af742c9430b572
Rasti2319 <rastih54@gmail.com>
2025-09-24 19:54:58 +0800

    Day17.java

---
+++ Day17.java
@@ -0,0 +1,13 @@
+public class ModulusExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        // Menggunakan modulus//
+        int hasil = a % b;
+
+        System.out.println("Bilangan a = " + a);
+        System.out.println("Bilangan b = " + b);
+        System.out.println("Sisa hasil bagi a % b = " + hasil);
+    }
+}
