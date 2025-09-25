72c5186025c66567ab96d213468eee53523ba37b
Rasti2319 <rastih54@gmail.com>
2025-09-25 21:48:21 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+	
+      //tipe data kecil(int)//
+              int angkaKecil = 60;    
+      //otomatis dikonversi ke long//
+            long angkaBesar = angkaKecil;
+      //otomatis dikonversi ke float
+        float pecahan = angkaBesar; 
+
+        System.out.println("Int   : " + angkaKecil);
+        System.out.println("Long  : " + angkaBesar);
+        System.out.println("Float : " + pecahan);
+    }
+}
