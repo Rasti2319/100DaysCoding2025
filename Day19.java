d4cddda16ac7555b7c35d009853e553d75035ff6
Rasti2319 <rastih54@gmail.com>
2025-09-26 20:13:15 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+   //int memiliki kapasitas besar//
+            int angkaBesar = 177;   
+     //konversi paksa ke byte//
+           byte angkaKecil = (byte) angkaBesar;
+
+        System.out.println("Angka int   : " + angkaBesar);
+        System.out.println("Angka byte  : " + angkaKecil);
+    }
+}
+	
