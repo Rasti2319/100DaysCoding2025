ebc3f7a13aba5d2f12a2bf4a36839b1f63f39add
Rasti2319 <rastih54@gmail.com>
2025-09-27 21:01:49 +0800

    Day20.java

--- Day20.java
+++ Day20.java
@@ -1,12 +1,21 @@
 public class Main {
 	public static void main(String[] args) {
-  //fata primitif
-       int angka = 100; 
-        // ubah menjadi String
-        String strAngka = String.valueOf(angka);
+          //data primitif
+              int angka = 100;
 
-        // tampilkan hasil
-        System.out.println("Hasil konversi: " + strAngka);
+       // 1. Menggunakan tanda kutip kosong ("")
+        String str1 = angka + "";
+        
+        // 2. Menggunakan String.valueOf()
+        String str2 = String.valueOf(angka);
+        
+        // 3. Menggunakan Integer.toString()
+        String str3 = Integer.toString(angka);
+
+        // Output hasil konversi
+        System.out.println("Cara 1: " + str1);
+        System.out.println("Cara 2: " + str2);
+        System.out.println("Cara 3: " + str3);
     }
 }
-	
+   
