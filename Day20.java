437d4077e407dce819aee1949fb56ef3ae98500c
Rasti2319 <rastih54@gmail.com>
2025-09-27 20:13:35 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+  //fata primitif
+       int angka = 100; 
+        // ubah menjadi String
+        String strAngka = String.valueOf(angka);
+
+        // tampilkan hasil
+        System.out.println("Hasil konversi: " + strAngka);
+    }
+}
+	
