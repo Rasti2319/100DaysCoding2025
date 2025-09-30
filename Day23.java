71403126414799a3522d170b8576c6c9428f6b05
Rasti2319 <rastih54@gmail.com>
2025-09-30 20:47:59 +0800

    Day23.java

---
+++ Day23.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		// nilai panjang dan lebar//
+		double panjang=10.2;
+		double lebar=4.3;
+		//hitung luas//
+		double luas=panjang*lebar;
+		//menampilkan hasil//
+		System.out.println("luas persegi panjang=" + luas + "cm");
+	}
+}
