961ab71c480138e758a8a1a5b5d86a923cd8e0f1
Rasti2319 <rastih54@gmail.com>
2025-10-01 20:17:27 +0800

    Day24.java

---
+++ Day24.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		//nilai jari-jari//
+		double JariJari = 3.0;
+		//Rumus lingkaran//
+		double luas = Math.PI * JariJari * JariJari;
+		//menampilkan hasil//
+		System.out.println("luas lingkaran=" + luas);
+	}
+}
