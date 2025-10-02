5f9458c0d0170365dbfbfb69c80f5c5d0ca78865
Rasti2319 <rastih54@gmail.com>
2025-10-02 20:57:39 +0800

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		int a = 8;
+		//menggunakan increment//
+		System.out.println("nilai awal a =" + a);
+		a++;
+		System.out.println("nilai a setelah increment=" + a);
+     int b = 7;
+   //menggunakan decrement//
+       System.out.println("nilai awal b =" + b);
+       b--;
+       System.out.println("nilai b setelah decrement =" + b);
+		}
+}
