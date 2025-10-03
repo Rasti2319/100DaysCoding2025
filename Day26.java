8731105e043f2125fe8fe2cd5b77b85c9181cc1a
Rasti2319 <rastih54@gmail.com>
2025-10-03 21:08:26 +0800

    Day26.java

---
+++ Day26.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) {
+		int a = 6;
+		a+=4;
+		System.out.println("hasil a=" + a);
+	   int b=5;
+   	b-=2;
+	System.out.println("hasil b=" + b);
+	   int c=4;
+		c*=4;
+		System.out.println("hasil c=" + c);
+		int d=8;
+		d/=4;
+		System.out.println("hasil d=" + d);
+	}
+}
