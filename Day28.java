e6ab5d0ae124dcb478a816521b4ecc4711958fb7
Rasti2319 <rastih54@gmail.com>
2025-10-05 21:57:36 +0800

    Day28.java

---
+++ Day28.java
@@ -0,0 +1,11 @@
+public class Main {
+	
+    public static void main(String[] args) {
+        int a = 12;
+        int  b = 10;
+
+        System.out.println("a==b:" + (a==b));//salah karena 12 tidak sama dengan 10//
+        
+        System.out.println("a!=b:" + (a!=b));//benar karena 12 tidak sama dengan 10//
+    }
+}
