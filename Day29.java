aac9513c07a8f6f30ca2ab8ef8884cf232e60f9e
Rasti2319 <rastih54@gmail.com>
2025-10-06 21:13:03 +0800

    Day29.java

---
+++ Day29.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		int a = 8;
+int b = 3;
+System.out.println(a > b); // true karena 8 lebih besar dari 3
+System.out.println(a < b); //false karena 8 lebih besar daripada 3
+	}
+}
