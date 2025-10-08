32648249e7bf12e58d19bfb3a956a895ae863a31
Rasti2319 <rastih54@gmail.com>
2025-10-08 22:17:31 +0800

    Day31.java

---
+++ Day31.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		boolean f = true ;
+		boolean g = false;
+		boolean h = true;
+		System.out.println(f && g);
+		System.out.println(f && h);
+	}
+}
